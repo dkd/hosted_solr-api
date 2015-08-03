2db108752768d69393e36fc260a15d9a32cad139
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2015-08-03 16:26:05 +0200

    added more specs

---
+++ spec/hosted_solr/configuration_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe HostedSolr::API::Configuration do
+  subject { HostedSolr::API::Configuration.new }
+
+  it 'has a default base_url pointing to hosted-solr.com' do
+    expect(subject.base_url).to eq 'https://www.hosted-solr.com/api'
+  end
+
+  it 'allows to set an API token' do
+    expect(subject).to respond_to(:api_token=)
+  end
+
+  it 'allows to set an Secret token' do
+    expect(subject).to respond_to(:secret_token=)
+  end
+
+  it 'allows to read the API token' do
+    subject.api_token = 'my API token'
+    expect(subject.api_token).to eq 'my API token'
+  end
+
+  it 'allows to read the Secret token' do
+    subject.secret_token = 'my Secret token'
+    expect(subject.secret_token).to eq 'my Secret token'
+  end
+
+  it 'falls back to env variable HOSTED_SOLR_API_TOKEN when reading the API token' do
+    ENV['HOSTED_SOLR_API_TOKEN'] = 'my API token via env'
+    expect(subject.api_token).to eq 'my API token via env'
+  end
+
+  it 'falls back to env variable HOSTED_SOLR_SECRET_TOKEN when reading the Secret token' do
+    ENV['HOSTED_SOLR_SECRET_TOKEN'] = 'my Secret token via env'
+    expect(subject.secret_token).to eq 'my Secret token via env'
+  end
+end
