5983265c4e62791525cbaeaaf1c536d0ae01303e
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2015-08-03 11:41:57 +0200

    added schema from API

--- lib/hosted_solr/api/solr_core.rb
+++ lib/hosted_solr/api/solr_core.rb
@@ -18,7 +18,7 @@ module HostedSolr
       validates :system,       presence: true
 
       def self.from_hash(attributes_hash = {})
-        new(attributes_hash.slice(*%w(id solr_version system name internal_name password is_activated created_at updated_at)))
+        new(attributes_hash.slice(*%w(id solr_version system schema name internal_name password is_activated created_at updated_at)))
       end
     end
   end
