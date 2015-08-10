7da98579084c3ee2099d7c027b09a1c84766298d
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2015-08-10 12:43:11 +0200

    added new 'host' attribute

--- spec/hosted_solr/solr_core_spec.rb
+++ spec/hosted_solr/solr_core_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe HostedSolr::API::SolrCore do
   subject { HostedSolr::API::SolrCore.new }
 
-  [:id, :solr_version, :system, :schema, :name, :internal_name, :password, :is_activated, :created_at, :updated_at].each do |attribute_name|
+  [:id, :solr_version, :system, :schema, :name, :internal_name, :password, :hostname, :is_activated, :created_at, :updated_at].each do |attribute_name|
     it "has an attribute #{attribute_name}" do
       expect(subject).to respond_to(attribute_name)
     end
@@ -15,7 +15,7 @@ describe HostedSolr::API::SolrCore do
     end
   end
 
-  [:id, :solr_version, :system, :schema, :name, :internal_name, :password, :is_activated, :created_at, :updated_at].each do |attribute_name|
+  [:id, :solr_version, :system, :schema, :name, :internal_name, :password, :hostname, :is_activated, :created_at, :updated_at].each do |attribute_name|
     it "accepts an attribute #{attribute_name} when initialize from a hash" do
       core = HostedSolr::API::SolrCore.from_hash(attribute_name.to_s => 'some value')
       expect(core.send(attribute_name)).to eq 'some value'
